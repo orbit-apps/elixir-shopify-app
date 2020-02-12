aa4ba44ee571dd104135a767186d206d55506f4e
Hez Ronningen <code@hez.ca>
2020-02-12 10:02:57 -0800

    add credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -35,6 +35,8 @@ defmodule ShopifyApp.MixProject do
     [
       # dev
       {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:credo, "~> 1.2.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
       # everything else
       {:ecto_sql, "~> 3.0"},
       {:gettext, "~> 0.11"},
