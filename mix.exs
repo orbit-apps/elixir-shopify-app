5345e4fb62dcc3437d3e182dcfb912def80a5653
Hez Ronningen <code@hez.ca>
2021-02-08 16:00:43 -0800

    bump to latest elixir ver

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ShopifyApp.MixProject do
     [
       app: :shopify_app,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
