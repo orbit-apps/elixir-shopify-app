0981661d0f491cb05f06f1af039227b191910ed1
Hez Ronningen <code@hez.ca>
2022-09-26 15:57:00 -0700

    import phx component as per docs
    https://hexdocs.pm/phoenix_live_view/installation.html#existing-projects

--- lib/shopify_app_web.ex
+++ lib/shopify_app_web.ex
@@ -37,6 +37,8 @@ defmodule ShopifyAppWeb do
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
+      import Phoenix.Component
+
       # Include shared imports and aliases for views
       unquote(view_helpers())
     end
@@ -72,6 +74,7 @@ defmodule ShopifyAppWeb do
       use Phoenix.Router
 
       import Plug.Conn
+      import Phoenix.Component
       import Phoenix.Controller
       import Phoenix.LiveView.Router
     end
