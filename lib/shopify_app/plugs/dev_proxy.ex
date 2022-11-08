8652943a45ff61937199fc5bca73ea0405aac1f0
Hez Ronningen <code@hez.ca>
2022-11-08 09:38:33 -0800

    reverse proxy sends response, so halt after it is called

--- lib/shopify_app/plugs/dev_proxy.ex
+++ lib/shopify_app/plugs/dev_proxy.ex
@@ -1,16 +1,25 @@
 defmodule ShopifyApp.Plug.DevProxy do
   if Mix.env() == :dev do
     require Logger
+    import Plug.Conn, only: [halt: 1], warn: false
     defdelegate init(opts), to: ReverseProxyPlug
 
     def call(%{request_path: "/manifest.json"} = conn, opts),
-      do: ReverseProxyPlug.call(conn, opts)
+      do: conn |> ReverseProxyPlug.call(opts) |> halt()
 
     def call(%{request_path: "/static/js/bundle.js"} = conn, opts),
-      do: ReverseProxyPlug.call(conn, opts)
+      do: conn |> ReverseProxyPlug.call(opts) |> halt()
 
-    def call(%{request_path: "/shop_admin" <> _} = conn, opts),
-      do: ReverseProxyPlug.call(conn, opts)
+    def call(%{request_path: "/shop_admin" <> _} = conn, opts) do
+      opts =
+        Keyword.put(opts, :error_callback, fn error ->
+          Logger.error("Network error: #{inspect(error)}")
+        end)
+
+      conn
+      |> ReverseProxyPlug.call(opts)
+      |> halt()
+    end
 
     def call(conn, _), do: conn
   else
