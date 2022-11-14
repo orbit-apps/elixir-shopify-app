49ed81bf5e185ceddf5a2f960a215ccfad1b1d48
Hez Ronningen <code@hez.ca>
2022-11-14 14:03:28 -0800

    add config module and refactor init code to use it

--- config/test.exs
+++ config/test.exs
@@ -31,3 +31,5 @@ config :logger, level: :warning
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :shopify_app, :shopify, api_key: "test", client_secret: "test"
