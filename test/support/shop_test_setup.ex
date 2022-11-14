d22f90aed10d8e32ec846bc708fc531c12d74f63
Hez Ronningen <code@hez.ca>
2022-11-14 14:04:13 -0800

    add /api/admin endpoint

---
+++ test/support/shop_test_setup.ex
@@ -0,0 +1,60 @@
+defmodule ShopifyApp.ShopTestSetup do
+  import ShopifyApp.Factory
+
+  def app_auth_token_shop(context) do
+    [shop: shop, shopifyapi_shop: _] = shop(context)
+    [app: app] = app(context)
+    [token: token] = auth_token(%{shop: shop})
+    [shop_admin_token: shop_admin_token] = shop_admin_token(%{shop: shop, app: app})
+
+    [
+      app: app,
+      auth_token: token,
+      shop: shop,
+      shop_admin_token: shop_admin_token
+    ]
+  end
+
+  def shop(context) do
+    shop =
+      case Map.get(context, :myshopify_domain) do
+        nil -> insert(:shop)
+        domain -> insert(:shop, myshopify_domain: domain)
+      end
+
+    shopifyapi_shop = %ShopifyAPI.Shop{domain: shop.myshopify_domain}
+    ShopifyAPI.ShopServer.set(shopifyapi_shop, false)
+
+    [shop: shop, shopifyapi_shop: shopifyapi_shop]
+  end
+
+  def app(_context) do
+    app = %ShopifyAPI.App{
+      name: ShopifyApp.Config.app_name(),
+      client_id: "#{__MODULE__}.id",
+      client_secret: "secret"
+    }
+
+    ShopifyAPI.AppServer.set(app)
+    [app: app]
+  end
+
+  def shop_admin_token(%{app: app, shop: shop}) do
+    payload = %{"aud" => app.client_id, "dest" => "https://#{shop.myshopify_domain}"}
+    jwk = JOSE.JWK.from_oct(app.client_secret)
+    {_, jwt} = jwk |> JOSE.JWT.sign(%{"alg" => "HS256"}, payload) |> JOSE.JWS.compact()
+
+    [shop_admin_token: jwt]
+  end
+
+  def auth_token(%{shop: %{myshopify_domain: myshopify_domain}}) do
+    token = %ShopifyAPI.AuthToken{
+      shop_name: myshopify_domain,
+      app_name: ShopifyApp.Config.app_name()
+    }
+
+    ShopifyAPI.AuthTokenServer.set(token, false)
+
+    [token: token]
+  end
+end
