b9b3df2d86b8a48614b70eb8d2f57a20cb9e1ed6
Hez Ronningen <code@hez.ca>
2022-11-14 14:03:43 -0800

    add find/1 to shops module for fetching

--- lib/shopify_app/shops.ex
+++ lib/shopify_app/shops.ex
@@ -12,8 +12,12 @@ defmodule ShopifyApp.Shops do
     |> Repo.insert_or_update()
   end
 
+  @spec find(String.t()) :: %Schema.Shop{} | nil
+  def find(myshopify_domain), do: Repo.get_by(Schema.Shop, myshopify_domain: myshopify_domain)
+
+  @spec find_or_new(map()) :: %Schema.Shop{}
   def find_or_new(%{myshopify_domain: myshopify_domain}) do
-    case Repo.get_by(Schema.Shop, myshopify_domain: myshopify_domain) do
+    case find(myshopify_domain) do
       nil -> %Schema.Shop{}
       shop -> shop
     end
