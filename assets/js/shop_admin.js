bce058e689d4571bc56652027fdc1523b4c663ae
Graham Baradoy <graham@baradoy.ca>
2023-09-26 07:40:29 -0700

    Add shopify.loading to shop_admin LiveView
    Adds using Shopify's loading bar. This requires splitting out the JS for shop admin into its own file.

---
+++ assets/js/shop_admin.js
@@ -0,0 +1,23 @@
+// JS specific to LiveView in ShopAdmin
+
+// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
+import "phoenix_html"
+// Establish Phoenix Socket and LiveView configuration.
+import {Socket} from "phoenix"
+import {LiveSocket} from "phoenix_live_view"
+
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+
+// Show progress bar on live navigation and form submits
+window.addEventListener("phx:page-loading-start", _info => shopify.loading(true))
+window.addEventListener("phx:page-loading-stop", _info => shopify.loading(false))
+
+// connect if there are any LiveViews on the page
+liveSocket.connect()
+
+// expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+// >> liveSocket.disableLatencySim()
+window.liveSocket = liveSocket
