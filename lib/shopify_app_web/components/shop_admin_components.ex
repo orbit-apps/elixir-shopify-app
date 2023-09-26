95f042bbfd68e42fc8e01f19c93bbb7123e4aa12
Graham Baradoy <graham@baradoy.ca>
2023-09-26 14:12:55 -0700

    Add toast messages when a message is put into flash

--- lib/shopify_app_web/components/shop_admin_components.ex
+++ lib/shopify_app_web/components/shop_admin_components.ex
@@ -19,4 +19,34 @@ defmodule ShopifyAppWeb.ShopAdminComponents do
     </div>
     """
   end
+
+  @doc """
+  Renders Shopify toast notices.
+
+  Uses ShopifyToastHook to integrate with AppBridge
+    https://shopify.dev/docs/api/app-bridge-library/reference/toast#example-toast-with-dismiss-callback
+
+  ## Examples
+
+      <.toast kind={:info} flash={@flash} />
+      <.toast kind={:error} flash={@flash} />
+  """
+  attr :id, :string, default: "toast", doc: "the optional id of toast container"
+  attr :flash, :map, default: %{}, doc: "the map of flash messages to display"
+  attr :kind, :atom, values: [:info, :error], doc: "used for styling and flash lookup"
+
+  def toast(assigns) do
+    ~H"""
+    <div
+      :if={msg = Phoenix.Flash.get(@flash, @kind)}
+      id={@id}
+      data-message={msg}
+      data-kind={@kind}
+      phx-hook="ShopifyToastHook"
+      hidden
+    >
+      <%= msg %>
+    </div>
+    """
+  end
 end
