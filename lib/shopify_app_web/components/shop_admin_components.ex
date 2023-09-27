e3df0042a8d99c0a259d891c8ec769f4ad0f9c68
Graham Baradoy <graham@baradoy.ca>
2023-09-27 08:12:09 -0700

    Change bindingPrefix for ShopAdmin to `data-phx-`
    AppBridge is incredibly picky about what goes into things like `<ui-title-bar/>` and will error if `phx-` binding are there. Instead we can use `data-phx-`

--- lib/shopify_app_web/components/shop_admin_components.ex
+++ lib/shopify_app_web/components/shop_admin_components.ex
@@ -42,11 +42,43 @@ defmodule ShopifyAppWeb.ShopAdminComponents do
       id={@id}
       data-message={msg}
       data-kind={@kind}
-      phx-hook="ShopifyToastHook"
+      data-phx-hook="ShopifyToastHook"
       hidden
     >
       <%= msg %>
     </div>
+    """
+  end
+
+  @doc """
+  Renders a button.
+
+  ## Examples
+
+      <.button>Send!</.button>
+      <.button data-phx-click="go" class="ml-2">Send!</.button>
+  """
+  attr :type, :string, default: nil
+  attr :class, :string, default: nil
+  attr :variant, :string, default: nil
+  attr :rest, :global, include: ~w(disabled form name value)
+
+  slot :inner_block, required: true
+
+  def button(assigns) do
+    ~H"""
+    <button
+      type={@type}
+      variant={@variant}
+      class={[
+        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
+        "text-sm font-semibold leading-6 text-white active:text-white/80",
+        @class
+      ]}
+      {@rest}
+    >
+      <%= render_slot(@inner_block) %>
+    </button>
     """
   end
 end
