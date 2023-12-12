925e2a7e9f1197abbf052db51b88bf7bd97e5fb3
Hez Ronningen <code@hez.ca>
2023-12-12 10:32:39 -0800

    refactor shops
    - use new repo macro for types
    - added delete funct

--- lib/shopify_app/shops.ex
+++ lib/shopify_app/shops.ex
@@ -1,25 +1,26 @@
 defmodule ShopifyApp.Shops do
   @moduledoc false
+  use ShopifyApp.Repo, define_types: ShopifyApp.Schema.Shop.t()
   alias ShopifyApp.Repo
   alias ShopifyApp.Schema
 
   def all, do: Repo.all(Schema.Shop)
 
-  def insert(shop) do
-    shop
-    |> find_or_new()
-    |> Schema.Shop.changeset(shop)
-    |> Repo.insert_or_update()
-  end
+  @spec insert(map()) :: ok_changeset_error()
+  def insert(shop),
+    do: shop |> find_or_new() |> Schema.Shop.changeset(shop) |> Repo.insert_or_update()
 
-  @spec find(String.t()) :: %Schema.Shop{} | nil
+  @spec find(String.t()) :: t() | nil
   def find(myshopify_domain), do: Repo.get_by(Schema.Shop, myshopify_domain: myshopify_domain)
 
-  @spec find_or_new(map()) :: %Schema.Shop{}
+  @spec find_or_new(map()) :: t()
   def find_or_new(%{myshopify_domain: myshopify_domain}) do
     case find(myshopify_domain) do
       nil -> %Schema.Shop{}
       shop -> shop
     end
   end
+
+  @spec delete(t()) :: ok_changeset_error()
+  def delete(shop) when is_struct(shop, Schema.Shop), do: Repo.delete(shop)
 end
